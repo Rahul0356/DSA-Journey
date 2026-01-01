ec46cb947c9d5ea7353141a2e1907ee1ae6125f0
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-01-01 11:16:58 +0530

    basic

---
+++ 01_basic/Programming_101.js
@@ -0,0 +1,19 @@
+// console.log("Hello Dsa")
+// console.log("Hey Rahul Arya")
+// console.log("How are you")
+
+
+// const a = 12;
+// const b= 10;
+// const sum = a+ b;
+
+// console.log(sum)
+
+let firstName ="Rahul";
+let lastName ="Arya";
+//resign
+lastName = "Kumar"
+
+let fullName = firstName + lastName;
+
+console.log( fullName)
