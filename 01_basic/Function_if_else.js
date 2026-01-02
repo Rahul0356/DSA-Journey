b339f2540e95530a5d28384a14fabf8cf118b4e0
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-01-02 10:16:44 +0530

    solved the add ,multiply,divide two number

---
+++ 01_basic/Function_if_else.js
@@ -0,0 +1,31 @@
+function greet(name){
+    console.log("Namaste," + name);
+}
+
+// greet("Rahul")
+
+// Add Two Numbers
+
+// Add Two Numbers
+
+function sum(a,b){
+    let add = a+b;
+    console.log(add)
+}
+sum(5,1)
+
+// multiply two numbers
+
+function multi (x,y){
+    let multi = x * y;
+    console.log(multi)
+}
+multi(6,3)
+
+// divide two numbers
+
+function divideNumber(a,b){
+    let divideNumber =  a /b;
+    console.log(divideNumber)
+}
+divideNumber(10,2)
