83efb74c48a451009c1f0bb6b41221ee1425f6ed
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-01-02 10:39:48 +0530

    question add

--- 01_basic/Voting_eligiable.js
+++ 01_basic/Voting_eligiable.js
@@ -1,3 +1,7 @@
+// Problem Statement:
+// Write a program that accepts a number (age) and checks whether the person is eligible to vote. A person is eligible if their age is 18 or more.
+
+
 let age = 25;
 if(age >= 18){
     console.log("You are able")
