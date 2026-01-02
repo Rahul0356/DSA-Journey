a524f1deecff7b93ac54835f3327069e134ec735
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-01-02 10:38:36 +0530

    mathod-2

--- 01_basic/Voting_eligiable.js
+++ 01_basic/Voting_eligiable.js
@@ -5,3 +5,15 @@ if(age >= 18){
 else {
     console.log("You are not able")
 }
+
+// Method-2
+
+function ageAble (age){
+    if(age>=18){
+        console.log("You are able")
+    }
+    else {
+        console.log("You are  not able")
+    }
+}
+ageAble(12)
