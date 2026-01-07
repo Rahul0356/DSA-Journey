da8a9de203fb7de9354f2951fed3504ffe41ef93
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-01-07 15:44:55 +0530

    count print

--- 03_star_pattern/triangle_number_pattern.js
+++ 03_star_pattern/triangle_number_pattern.js
@@ -1,4 +1,9 @@
-let n= 5;
-for(let i=0; i<n; i++){
-    let row = ""
+
+let n = 4;
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j <= i; j++) {
+    row += (j + 1);
+  }
+  console.log(row);
 }
