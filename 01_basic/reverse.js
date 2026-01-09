73bde543fa0de60fc2cab018146ef1440dca567a
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-01-10 01:49:02 +0530

    reverse number

---
+++ 01_basic/reverse.js
@@ -0,0 +1,18 @@
+var reverse = function(x){
+
+    let xCopy = x;
+    x = Math.abs(x);
+
+    let rev = 0;
+    while(x>0){
+
+        let last = x%10;
+        rev = rev *10 + last;
+        x = Math.floor(x/10);
+    }
+    if(rev > 2 ** 31 -1) return 0;
+    return xCopy < 0 ? -rev : rev;
+}
+console.log(reverse(-1234));
+console.log(reverse(1200));
+console.log(reverse(1534236469));
