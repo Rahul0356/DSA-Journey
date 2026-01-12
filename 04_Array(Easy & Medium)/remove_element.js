4d40e51720e89ae075582137725ed9f6867c3463
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-01-12 18:21:55 +0530

    solve remove element in arrary

---
+++ 04_Array(Easy & Medium)/remove_element.js
@@ -0,0 +1,11 @@
+var removeElemetn = function(nums, val) {
+    let x = 0;
+    for(let i=0; i<nums.length;i++){
+        if(nums[i] != val){
+            nums[x] = nums[i];
+            x++;
+        }
+    }
+    return x;
+};
+console.log(removeElemetn([3,2,2,3],3))
