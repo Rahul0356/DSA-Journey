cb4fef67938fa4d8f147392125787308d3b4d23c
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-01-12 18:27:42 +0530

    reverse string

---
+++ 04_Array(Easy & Medium)/reverse_string.js
@@ -0,0 +1,13 @@
+var reverseString = function(s){
+    let  len = s.length;
+    let halfLen = Math.floor(len/2);
+
+    for(let i=0;i<halfLen;i++){
+        let temp = s[i];
+        s[i] = s[len-1-i];
+        s[len-1-i] = temp;
+    }
+}
+let arr = ["R", "a", "h", "u", "l"];
+reverseString(arr);
+console.log(arr);
