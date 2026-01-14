ab9af56b187fd9f2dcbc2a15baaa6e21081cb2ec
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-01-14 17:57:50 +0530

    max consecutive ones

---
+++ 04_Array(Easy & Medium)/max_consecutive_Ones.js
@@ -0,0 +1,16 @@
+var findMaxConsecutiveOnes = function(nums) {
+    let currentCount = 0;
+    let maxCount = 0;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] == 1) {
+        currentCount++;
+      } else {
+        maxCount = Math.max(currentCount, maxCount);
+        currentCount = 0;
+      }
+    }
+    return Math.max(maxCount, currentCount);
+  };
+    
+  const nums = [1, 1, 0, 1, 1, 1];
+console.log(findMaxConsecutiveOnes(nums));
